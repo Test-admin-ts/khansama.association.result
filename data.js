e6f76954500778fd67a4f8730e35e3076ea673ef
Test-admin-ts <toufik.sarwar3@gmail.com>
2026-01-18 11:16:43 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,611 @@
+const students = [
+  {
+    roll: "101",
+    name: "রাহিম ইসলাম",
+    class: "5",
+    school: "abc Kindergarten",
+    scholarship: "Talent Pool"
+  },
+  {
+    roll: "102",
+    name: "করিম উদ্দিন",
+    class: "5",
+    school: "Sunshine Kindergarten",
+    scholarship: "General Grade"
+  },
+  {
+  roll: "1122",
+  name: "মোঃ কেয়া মনি",
+  class: "5",
+  school: "সোনার তরী মডেল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1123",
+  name: "মোঃ লিয়ানা আক্তার",
+  class: "5",
+  school: "সোনার তরী মডেল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1124",
+  name: "মোঃ তাসনিমা আক্তার মনিরা",
+  class: "5",
+  school: "সোনার তরী মডেল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1270",
+  name: "সিনহা আক্তার",
+  class: "5",
+  school: "সোনার তরী মডেল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1183",
+  name: "মোঃ মাহিদ হাসান মাহিম",
+  class: "5",
+  school: "কাচিনিয়া আইডিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1073",
+  name: "আলিফ মোঃ শাহরিয়ার হাসান",
+  class: "5",
+  school: "ব্রাইট ফি স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1158",
+  name: "কাব্য চাঁদ রায়",
+  class: "5",
+  school: "রিলিফ পাবলিক মডেল স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1161",
+  name: "মোঃ লাবিব হোসেন",
+  class: "5",
+  school: "রিলিফ পাবলিক মডেল স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1166",
+  name: "মুশফিকুর রহমান আবিদ",
+  class: "5",
+  school: "রিলিফ পাবলিক মডেল স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1168",
+  name: "মোঃ আরাফাত হোসেন",
+  class: "5",
+  school: "রিলিফ পাবলিক মডেল স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1171",
+  name: "মোঃ রিমন হোসেন",
+  class: "5",
+  school: "রিলিফ পাবলিক মডেল স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1172",
+  name: "লিংকন রায় দেবশীল",
+  class: "5",
+  school: "রিলিফ পাবলিক মডেল স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1033",
+  name: "মোঃ রফিউন্নবী রাফি",
+  class: "5",
+  school: "উইনার রেসিডেন্সিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1034",
+  name: "মোঃ আরমান হোসেন",
+  class: "5",
+  school: "উইনার রেসিডেন্সিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1049",
+  name: "বন্যা রায়",
+  class: "5",
+  school: "উইনার রেসিডেনশিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1001",
+  name: "মরিয়ম আখতার",
+  class: "5",
+  school: "রয়েল স্টার স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1008",
+  name: "মোঃ আইরিন পারভীন",
+  class: "5",
+  school: "রয়েল স্টার স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1010",
+  name: "জাকিয়া তুজ জহরা",
+  class: "5",
+  school: "রয়েল স্টার স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1017",
+  name: "মোঃ আমিরুল মুমিনীন মুনিম",
+  class: "5",
+  school: "রয়েল স্টার স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1018",
+  name: "মোঃ আল আমিন ইসলাম",
+  class: "5",
+  school: "রয়েল স্টার স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1019",
+  name: "মোঃ তাকিউদ্দি ইসলাম ত্বহা",
+  class: "5",
+  school: "রয়েল স্টার স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1195",
+  name: "লাবীব মোহাম্মদ",
+  class: "5",
+  school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1196",
+  name: "তিতাস রায়",
+  class: "5",
+  school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1197",
+  name: "মাহিয়ান মুন",
+  class: "5",
+  school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1198",
+  name: "রাব্বি ইসলাম",
+  class: "5",
+  school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1199",
+  name: "জিৎ রায়",
+  class: "5",
+  school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1201",
+  name: "সৈকত  ইসলাম",
+  class: "5",
+  school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1230",
+  name: "মিসরাত জাহান",
+  class: "5",
+  school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1231",
+  name: "নন্দিতা রায়",
+  class: "5",
+  school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1335",
+  name: "ইসরাত জাহান",
+  class: "5",
+  school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1244",
+  name: "মোঃ মুজাহিদ হোসেন নূর",
+  class: "5",
+  school: "সানরাইজ আইডিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1246",
+  name: "বর্ণ রায়",
+  class: "5",
+  school: "সানরাইজ আইডিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+
+{
+  roll: "1271",
+  name: "মোঃ রফিক ইসলাম চৌধুরী",
+  class: "5",
+  school: "সানরাইজ আইডিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1258",
+  name: "করবী রানী রায় বর্ষা",
+  class: "5",
+  school: "সানরাইজ আইডিয়াল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1100",
+  name: "আলাল উদ্দিন",
+  class: "5",
+  school: "স্টুডেন্টস কেয়ার স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1102",
+  name: "আবিদ আহমেদ",
+  class: "5",
+  school: "স্টুডেন্টস কেয়ার স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1098",
+  name: "টিটোন রায়",
+  class: "5",
+  school: "স্টুডেন্টস কেয়ার স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1068",
+  name: "মোঃ মুশফিকুর রহমান আবিদ",
+  class: "5",
+  school: "সুকুমার কিন্ডারগার্টেন এন্ড মাধ্যমিক বিদ্যালয়",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1088",
+  name: "রেশি মনি",
+  class: "5",
+  school: "শিশু কানন বিদ্যানিকেতন",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1261",
+  name: "মোছাঃ মরিয়ম আক্তার মারিয়া",
+  class: "5",
+  school: "নিউ ভিশন রেসিডেন্সিয়াল মডেল স্কুল",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1128",
+  name: "মোছাঃ রিসুমনি আক্তার",
+  class: "5",
+  school: "হলি চাইল্ড স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1131",
+  name: "মোঃ সাদিক আল গালিব",
+  class: "5",
+  school: "হলি চাইল্ড স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1078",
+  name: "কাজী মোবাশ্বীরুল হক",
+  class: "5",
+  school: "গ্রীন লিফ কিন্ডারগার্টেন",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1081",
+  name: "আরিফা জান্নাত আলিফ",
+  class: "5",
+  school: "গ্রীন লিফ কিন্ডারগার্টেন",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1137",
+  name: "বিষান দাস",
+  class: "5",
+  school: "অবজারভেন্ট রেসি. পাব. স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1138",
+  name: "মোঃ আব্দুল্লাহ আল-গালিব",
+  class: "5",
+  school: "অবজারভেন্ট রেসি. পাব. স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1139",
+  name: "সাদ আব্দুল্লাহ",
+  class: "5",
+  school: "অবাকাওয়েট রেসি. পাব. স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+{
+  roll: "1153",
+  name: "মোছাঃ নুসরাত জান্নাত",
+  class: "5",
+  school: "অবজারভেন্ট রেসি. পাব. স্কুল এন্ড কলেজ",
+  scholarship: "Talent Pool"
+},
+
+
+{
+roll: "1113",
+name: "কনক রায়",
+class: "5",
+school: "সোনার তরী মেডেল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1121",
+name: "মোহাঃ হুমায়রা আক্তার ওয়াসেনাত",
+class: "5",
+school: "সোনার তরী মেডেল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1125",
+name: "তামান্না আক্তার",
+class: " 5",
+school: "সোনার তরী মেডেল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1187",
+name: "ধৃতি রানী রায়",
+class: "5",
+school: "কাচিনীয়া আইডিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1072",
+name: "মোহাঃ সুরাইয়া আক্তার",
+class: "5",
+school: "ব্রাইট ফিউচার রেসিডেন্সিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1162",
+name: "মারজুক আহমেদ",
+class: "5",
+school: "রিলিফ পাবলিক মডেল স্কুল এন্ড কলেজ",
+scholarship: "General "
+},
+{
+roll: "1169",
+name: "উৎসব রায়",
+class: "5",
+school: "রিলিফ পাবলিক মডেল স্কুল এন্ড কলেজ",
+scholarship: "General "
+},
+{
+roll: "1180",
+name: "প্রিয়াঙ্কা রায়",
+class: "5",
+school: "রিলিফ পাবলিক মডেল স্কুল এন্ড কলেজ",
+scholarship: "General "
+},
+{
+roll: "1173",
+name: "মোঃ সারব্বির হোসেন",
+class: "5",
+school: "রিলিফ পাবলিক মডেল স্কুল এন্ড কলেজ",
+scholarship: "General "
+},
+{
+roll: "1193",
+name: "মোবাশ্বিরা আকতার মাহিরা",
+class: "5",
+school: "রিলিফ পাবলিক মডেল স্কুল এন্ড কলেজ",
+scholarship: "General "
+},
+{
+roll: "1035",
+name: "মোঃ লাফিন ইসলাম",
+class: "5",
+school: "উইনার রেসিডেন্সিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1048",
+name: "বন্যা রানী দাস",
+class: "5",
+school: "উইনার রেসিডেন্সিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1009",
+name: "মোছাঃ সালমা সাবিহা",
+class: "5",
+school: "রয়েল স্টার স্কুল",
+scholarship: "General "
+},
+
+
+{
+roll: "1013",
+name: "মোছাঃ মিম আকার",
+class: "5",
+school: "রয়েল স্টার স্কুল",
+scholarship: "General "
+},
+{
+roll: "1014",
+name: "মোছাঃ শিলা আক্তার",
+class: "5",
+school: "রয়েল স্টার স্কুল",
+scholarship: "General "
+},
+{
+roll: "1022",
+name: "রাজ আহমেদ শেখর",
+class: "5",
+school: "রয়েল স্টার স্কুল",
+scholarship: "General "
+},
+{
+roll: "1200",
+name: "শাহিন ইসলাম",
+class: "5",
+school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1204",
+name: "গোপনাথ রায়",
+class: "5",
+school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1202",
+name: "বিপ্লব রায়",
+class: "5",
+school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1233",
+name: "সামিরা আক্তার",
+class: "5",
+school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1227",
+name: "মোরসালিন ইসলাম",
+class: "5",
+school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1232",
+name: "পুষ্টি রায়",
+class: "5",
+school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1240",
+name: "মাহবুবা আক্তার",
+class: "5",
+school: "মেরিট ক্রিয়েটিভ রেসিডেন্সিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1242",
+name: "সাদ্দাদ হোসেন সায়েম",
+class: "5",
+school: "সানরাইজ আইডিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1252",
+name: "মোছাঃ রাফিয়া আক্তার",
+class: "5",
+school: "সানরাইজ আইডিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1255",
+name: "ফারজানা আক্তার বেবী",
+class: "5",
+school: "সানরাইজ আইডিয়াল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1099",
+name: "সুমন ইসলাম",
+class: "5",
+school: "স্টুডেন্টস কেয়ার  স্কুল এন্ড কলেজ",
+scholarship: "General "
+},
+{
+roll: "1101",
+name: "বিশ্বজিৎ রায় রকি",
+class: "5",
+school: "স্টুডেন্টস কেয়ার স্কুল এন্ড কলেজ",
+scholarship: "General "
+},
+{
+roll: "1067",
+name: "মোছাঃ মুশফিকা নাজনীন মাহি",
+class: "5",
+school: "সুকুমার কিন্ডারগার্টেন এন্ড মাধ্যমিক বিদ্যালয়",
+scholarship: "General "
+},
+
+
+{
+roll: "1090",
+name: "নিহারিকা রানী",
+class: "5",
+school: "শিশু কানন বিদ্যানিকেতন",
+scholarship: "General "
+},
+{
+roll: "1259",
+name: "মোছাঃ সুমাইয়া আক্তার সানজিদা",
+class: "5",
+school: "নিউ ভিশন রেসিডেন্সিয়াল মডেল স্কুল",
+scholarship: "General "
+},
+{
+roll: "1136",
+name: "মোছাঃ মরিয়ম আক্তার",
+class: "5",
+school: "হলি চাইন্ড স্কুল এন্ড কলেজ",
+scholarship: "General "
+},
+{
+roll: "1079",
+name: "আব্দুর রউফ",
+class: "5",
+school: "গ্রীন লিফ কিন্ডারগার্টেন",
+scholarship: "General "
+},
+{
+roll: "1151",
+name: "মোছাঃ মাইশা আক্তার",
+class: "5",
+school: "অবজারভেন্ট রেসি. পাব. স্কুল এন্ড কলেজ",
+scholarship: "General "
+},
+{
+roll: "1152",
+name: "খাদিজা আক্তার মোনা",
+class: "5",
+school: "অবজারভেন্ট রেসি. পাব. স্কুল এন্ড কলেজ",
+scholarship: "General "
+},
+{
+roll: "1156",
+name: "তাকিয়া আফরোজ",
+class: "5",
+school: "অবজারভেন্ট রেসি. পাব. স্কুল এন্ড কলেজ",
+scholarship: "General "
+}
+];
